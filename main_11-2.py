d1b074ea4270ac4395f0acd5bd678dccb7603694
petmetic <101994210+petmetic@users.noreply.github.com>
2022-05-08 14:03:30 +0200

    lesson-11-2_correction where def score_data lies

--- main_11-2.py
+++ main_11-2.py
@@ -14,26 +14,27 @@ with open("score_list.json", "r") as score_file:
 current_time = datetime.datetime.now()
 # print(current_time)
 
-score_data = {"attempts": attempts,
-              "wrong_attempts": wrong_guess,
-              "date": str(datetime.datetime.now()),
-              "name": name_of_player
-              }
-print(secret)
+
 while True:
     guess = int(input("Guess the secret number (between 1 and 30): "))
     attempts += 1
     wrong_attempts = +1
+
+    score_data = {"attempts": attempts,
+                  "wrong_attempts": wrong_guess,
+                  "date": str(datetime.datetime.now()),
+                  "name": name_of_player
+                  }
 
     if guess == secret:
         score_list.append(score_data)
 
         with open("score_list.json", "w") as score_file:
             score_file.write(json.dumps(score_list))
-            # score_file.write("\n With kind regards...")
-        print("You've guessed it - congratulations! It's number " + str(secret))
-        print("Attempts needed: " + str(attempts))
-        break
+    # score_file.write("\n With kind regards...")
+            print("You've guessed it - congratulations! It's number " + str(secret))
+            print("Attempts needed: " + str(attempts))
+            break
 
     elif guess > secret:
         wrong_guess = +1
